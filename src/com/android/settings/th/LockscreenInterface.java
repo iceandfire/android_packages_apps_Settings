f21389967bd0d2a79eff20dbc5a51e3b9e6772cb
David van Tonder <david.vantonder@gmail.com>
2012-05-15 22:28:10 -0400

    Lockscreen weather :D plus fixed conflicts with lockscreen slider customization

---
+++ src/com/android/settings/th/LockscreenInterface.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2012 CyanogenMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.th;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "LockscreenInterface";
+
+    public static final String KEY_WEATHER_PREF = "lockscreen_weather";
+    private Preference mWeatherPref;
+    ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mResolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.display_settings);
+        mWeatherPref = (Preference) findPreference(KEY_WEATHER_PREF);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        updateState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void updateState() {
+        // Set the weather description text
+        if (mWeatherPref != null) {
+            boolean weatherEnabled = Settings.System.getInt(mResolver,
+                    Settings.System.LOCKSCREEN_WEATHER, 0) == 1;
+            if (weatherEnabled) {
+                mWeatherPref.setSummary(R.string.lockscreen_weather_enabled);
+            } else {
+                mWeatherPref.setSummary(R.string.lockscreen_weather_summary);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+
+        return true;
+    }
+}
